147a6201aa29cc3e000419a3c45c2bfe36ce3d96
chandrasekharamavasya-eng <chandrasekharamavasya@gmail.com>
2026-09-02 12:49:16 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,39 @@
+a=10
+b=3
+print("Addtion:",a+b)
+print("Substraction:",a-b)
+print("Multiplicaation:",a*b)
+print("Division:",a/b)
+print("floor division:",a//b)
+print("Remainder:",a%b)
+print("power:",a**b)
+#siple calculator
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+print("Addtion:",a+b)
+print("Substraction:",a-b)
+print("Multiplicaation:",a*b)
+print("Division:",a/b)
+
+#student marks calculator
+name= input("enter student name:")
+m1= int(input("enter python marks:"))
+m2= int(input("enterjava marks:"))
+m3= int(input("enter sql mareks:"))
+total=m1+m2+m3
+average=total/3
+print("\n---student mark report---")
+print("name:",name)
+print("total marks:" , total)
+print("average marks:" ,average)
+
+#shopping bill caculator
+price1=float(input("enter product 1 price:"))
+price2=float(input("enter product 2 price:"))
+price3=float(input("enter product 3 price:"))
+ total = price1 +price2 + price3
+ discount= total* 0.10
+ final amount= total - discount
+ print("discount:"discount)
+ print("finacial")
